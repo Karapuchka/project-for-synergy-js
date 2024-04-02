5042a0fe769015a1e046013ce80d503485841aa7
Karapuchka <karapu4ka2@gmail.com>
2024-04-02 22:25:18 +0300

    .

--- gulp/source/scripts/main.js
+++ gulp/source/scripts/main.js
@@ -1,1 +1,19 @@
-let detect = new MobileDetect(window.navigator.userAgent);
+const btnLogin = document.getElementById('js-btn-login');
+
+gsap.to(btnLogin, {background: 'linear-gradient(45deg, #C89AFC, #7C6AFA)'});
+
+btnLogin.addEventListener('pointerover', ()=>{
+    gsap.to(btnLogin, {background: 'linear-gradient(45deg, #7C6AFA, #C89AFC)', duration: .7});
+});
+
+btnLogin.addEventListener('pointerout', ()=>{
+    gsap.to(btnLogin, {background: 'linear-gradient(45deg, #C89AFC, #7C6AFA)', duration: .7});
+});
+
+btnLogin.addEventListener('pointerdown', ()=>{
+    gsap.to(btnLogin, {background: 'linear-gradient(45deg, #7C6AFA, #C89AFC)', duration: .3});
+});
+
+btnLogin.addEventListener('pointerup', ()=>{
+    gsap.to(btnLogin, {background: 'linear-gradient(45deg, #C89AFC, #7C6AFA)', duration: .3});
+});
