2e13f440c5cd8de8106eaf9839e68b1630d8e4b4
Karapuchka <karapu4ka2@gmail.com>
2024-04-09 22:42:07 +0300

    .

--- gulp/source/scripts/main.js
+++ gulp/source/scripts/main.js
@@ -1,5 +1,7 @@
 const btnLogin = document.getElementById('js-btn-login');
+const btnDef = document.querySelectorAll('.js-btn-def');
 
+// Animation for btn login
 gsap.to(btnLogin, {background: 'linear-gradient(45deg, #C89AFC, #7C6AFA)'});
 
 btnLogin.addEventListener('pointerover', ()=>{
@@ -17,3 +19,27 @@ btnLogin.addEventListener('pointerdown', ()=>{
 btnLogin.addEventListener('pointerup', ()=>{
     gsap.to(btnLogin, {background: 'linear-gradient(45deg, #C89AFC, #7C6AFA)', duration: .3});
 });
+
+//Animation for default btn\
+
+gsap.to(btnDef, {background: 'linear-gradient(45deg, #DF5950, #451046)'});
+
+for (let i = 0; i < btnDef.length; i++) {
+
+    btnDef[i].addEventListener('pointerover', ()=>{
+        gsap.to(btnDef[i], {background: 'linear-gradient(45deg, #451046, #DF5950)', duration: .7});
+    });
+
+    btnDef[i].addEventListener('pointerout', ()=>{
+        gsap.to(btnDef[i], {background: 'linear-gradient(45deg, #DF5950, #451046)', duration: .7});
+    });
+
+    btnDef[i].addEventListener('pointerdown', ()=>{
+        gsap.to(btnDef[i], {background: 'linear-gradient(45deg, #451046, #DF5950)', duration: .3});
+    });
+
+    btnDef[i].addEventListener('pointerup', ()=>{
+        gsap.to(btnDef[i], {background: 'linear-gradient(45deg, #DF5950, #451046)', duration: .3});
+    });
+    
+}
